3446f419deaabd0a5c9ecba04ad6567d73701917
Varsha <varsha.cg35@gmail.com>
2021-09-15 18:06:54 +0530

    excel to json coversion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'convert-excel-to-json';
+  json! : string;
+
+  fileUpload(event:any){
+    console.log(event.target.files);
+    const selectedFile = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFile);
+    fileReader.onload = (event) =>{
+      console.log(event);
+      let binaryData = event.target?.result;
+      let workBook = XLSX.read(binaryData, {type : 'binary'});
+      console.log(workBook)
+      workBook.SheetNames.forEach(sheet =>{
+        const data = XLSX.utils.sheet_to_json(workBook.Sheets[sheet]);
+        console.log(data);
+        this.json = JSON.stringify(data, undefined, 4);
+      })
+    }
+  }
 }
